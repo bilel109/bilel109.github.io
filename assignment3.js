7ba064eebe298a38869ea28551df97fc44291a2d
bilel109 <122753002+bilel109@users.noreply.github.com>
2023-01-18 04:42:13 -0600

    update

---
+++ assignment3.js
@@ -0,0 +1,23 @@
+var names=new Array();
+names[0]="bilel";
+names[1]="jecem";
+names[2]="jayleni";
+names[3]="jamel";
+names[4]="hasni";
+names[5]="joumia";
+names[6]="mofid";
+names[7]="semi";
+names[8]="rayen";
+names[9]="ines";
+names[10]="jorjina";
+
+for ( var i = 0; i <names.length; i++){
+    if (names[i].charAt(0)==='j'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye"+names[i])
+    }
+    else{
+        console.log("hello"+names[i])
+    }
+
+}
+
